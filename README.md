3c0b022d5b6ee3cc5c978db80e086d594781fee1
riazahmedshah <riyazsh360@gmail.com>
2026-06-16 14:15:30 +0530

    add: db setup and README

---
+++ README.md
@@ -0,0 +1,1 @@
+#Booking application in GO
--- database/database.go
+++ database/database.go
@@ -1,1 +1,50 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/riazahmedshah/go-booking/config"
+)
+
+var Pool *pgxpool.Pool
+
+const DatabasePingTimeout = 10
+
+func New(cfg *config.Config) (*pgxpool.Pool, error) {
+	hostport := net.JoinHostPort(cfg.Database.Host, cfg.Database.Port)
+
+	encodedPassword := url.QueryEscape(cfg.Database.Password)
+
+	dns := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode%s",
+		cfg.Database.User,
+		encodedPassword,
+		hostport,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
+
+	pgxPoolConfig, err := pgxpool.ParseConfig(dns)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pgx pool config: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), pgxPoolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pgx pool: %w", err)
+	}
+
+	Pool = pool
+
+	ctx, cancel := context.WithTimeout(context.Background(), DatabasePingTimeout*time.Second)
+	defer cancel()
+	if err = pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return Pool, nil
+}
