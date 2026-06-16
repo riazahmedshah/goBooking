3876d3259c2a933539586cd0001f5ff0c3504c6a
riazahmedshah <riyazsh360@gmail.com>
2026-06-16 22:09:38 +0530

    add: migrator and migrations

---
+++ database/migrator.go
@@ -0,0 +1,64 @@
+package database
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"net"
+	"net/url"
+
+	"github.com/riazahmedshah/go-booking/config"
+
+	"github.com/jackc/pgx/v5"
+	tern "github.com/jackc/tern/v2/migrate"
+)
+
+//go:embed migrations/*.sql
+var migrations embed.FS
+
+func Migrate(ctx context.Context, cfg *config.Config) error {
+	hostPort := net.JoinHostPort(cfg.Database.Host, cfg.Database.Port)
+
+	// URL-encode the password
+	encodedPassword := url.QueryEscape(cfg.Database.Password)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		cfg.Database.User,
+		encodedPassword,
+		hostPort,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
+
+	conn, err := pgx.Connect(ctx, dsn)
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+
+	m, err := tern.NewMigrator(ctx, conn, "schema_version")
+	if err != nil {
+		return fmt.Errorf("constructing database migrator: %w", err)
+	}
+	subtree, err := fs.Sub(migrations, "migrations")
+	if err != nil {
+		return fmt.Errorf("retrieving database migrations subtree: %w", err)
+	}
+	if err := m.LoadMigrations(subtree); err != nil {
+		return fmt.Errorf("loading database migrations: %w", err)
+	}
+	from, err := m.GetCurrentVersion(ctx)
+	if err != nil {
+		return fmt.Errorf("retreiving current database migration version")
+	}
+	if err := m.Migrate(ctx); err != nil {
+		return err
+	}
+	if from == int32(len(m.Migrations)) {
+		slog.Info("database schema up to date", "version", len(m.Migrations))
+	} else {
+		slog.Info("migrated database schema", "from", from, "to", len(m.Migrations))
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,17 +5,28 @@ go 1.26.4
 require (
 	github.com/go-playground/validator/v10 v10.30.3
 	github.com/jackc/pgx/v5 v5.10.0
+	github.com/jackc/tern/v2 v2.4.1
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.3.0 // indirect
+	github.com/Masterminds/sprig/v3 v3.3.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
 	golang.org/x/crypto v0.52.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
